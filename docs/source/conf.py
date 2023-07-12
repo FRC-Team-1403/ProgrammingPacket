cfbbbae926146777694cb28fd73f2dd6757ef4e2
Etaash Mathamsetty <45927311+Etaash-mathamsetty@users.noreply.github.com>
2023-07-12 19:58:19 -0400

    change project name and author

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Programming Packet'
+copyright = '2023, Etaash Mathamsetty'
+author = 'Etaash Mathamsetty'
 
 release = '0.1'
 version = '0.1.0'
